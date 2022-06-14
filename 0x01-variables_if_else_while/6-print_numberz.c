e2a3836b591d73ef621cde3d97a075860a83dc6d
banga19 <bangalifof16@gmail.com>
2022-06-14 23:10:06 +0300

    adding task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ * main - prints single digit numebrs
+ *
+ * Description: print single digit numbers
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	return (0);
+}
