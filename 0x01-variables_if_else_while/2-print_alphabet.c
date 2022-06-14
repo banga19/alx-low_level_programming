9030a7f2929315c5b7826f5ccb48bbbdca4df611
banga19 <bangalifof16@gmail.com>
2022-06-14 14:24:45 +0300

    adding file 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * main - function prints alphabets from a to z in lowercase
+ *
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for( ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
